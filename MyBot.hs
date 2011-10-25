167c68499b5a0ee9343e7e7ed35c42056d16d83f
Mats Rauhala <mats.rauhala@gmail.com>
2011-10-25 18:01:58 +0300

    First draft of path finding

--- MyBot.hs
+++ MyBot.hs
@@ -3,8 +3,28 @@ module Main where
 import Data.List
 import Data.Maybe (mapMaybe)
 import System.IO
+import qualified Data.PQueue.Min as PQ
+import Control.Arrow
 
 import Ants
+
+type Heuristics = Int
+data Path = Path Heuristics Direction Point deriving (Eq, Ord)
+
+-- For now we don't need accurate distances
+simpleDistance :: Point -> Point -> Int
+simpleDistance (x1,y1) (x2,y2) = abs $ (x2-x1) + (y2-y1)
+
+path :: Point -> Point -> World -> [Direction]
+path origin dest world = map (\(Path _ d _) -> d) $ go [] $ PQ.fromList $ open origin
+  where
+    open point = [Path (simpleDistance (move dir point) point) dir (move dir point) | dir <- [North .. West], walkable world (move dir point)]
+    go :: [Path] -> PQ.MinQueue Path -> [Path]
+    go path queue =
+      let p@(Path _ _ point) = PQ.findMin queue
+          newpath = path ++ [p]
+          newqueue = PQ.union (PQ.deleteMin queue) $ PQ.fromList $ open point
+      in if point == dest then newpath else go newpath newqueue
 
 -- | Picks the first "passable" order in a list
 -- returns Nothing if no such order exists
